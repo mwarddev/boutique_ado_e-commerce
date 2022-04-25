26f0df665336ce7a97994c74b5d80b086dc52348
mwarddev <martinward1981@googlemail.com>
2022-04-25 04:16:27 +0000

    Update allauth templates and test profiles

--- profiles/models.py
+++ profiles/models.py
@@ -1,13 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django_countries.fields import CountryField
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+
+from django_countries.fields import CountryField
 
 
 class UserProfile(models.Model):
     """
-    A user model for maintaining default
+    A user profile model for maintaining default
     delivery information and order history
     """
     user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -22,13 +23,13 @@ class UserProfile(models.Model):
     def __str__(self):
         return self.user.username
 
-    @receiver(post_save, sender=User)
-    def create_or_update_user_profile(sender, instance, created, **kwargs):
-        """
-        Create or update the user profile
-        """
-        if created:
-            UserProfile.objects.create(user=instance)
-        # Existing users: just save the profile
-        instance.UserProfile.save()
 
+@receiver(post_save, sender=User)
+def create_or_update_user_profile(sender, instance, created, **kwargs):
+    """
+    Create or update the user profile
+    """
+    if created:
+        UserProfile.objects.create(user=instance)
+    # Existing users: just save the profile
+    instance.userprofile.save()
